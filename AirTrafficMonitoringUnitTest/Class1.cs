1aa0f0dcc337cdb77246fbce6f05e52ddcaf1d8c
ChristianMorup <chrmorup@gmail.com>
2018-04-03 09:02:30 +0200

    Unit test project added

---
+++ AirTrafficMonitoringUnitTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoringUnitTest
+{
+    public class Class1
+    {
+    }
+}
