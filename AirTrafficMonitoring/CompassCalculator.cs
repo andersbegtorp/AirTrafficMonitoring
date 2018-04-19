fa800d3d6602b30f0e8c61edb742d27ce4c040ab
MikkelHarbo <mikkelharbo32@gmail.com>
2018-04-19 13:46:13 +0200

    Unit test på CourseAnalyser og CompassCalculator

--- AirTrafficMonitoring/CompassCalculator.cs
+++ AirTrafficMonitoring/CompassCalculator.cs
@@ -12,15 +12,13 @@ namespace AirTrafficMonitoring
             var deltaX = x2 - x1;
             var deltaY = y2 - y1;
 
-            var atan = Math.Atan(deltaY / deltaX) / Math.PI * 180;
-            if (deltaX < 0 || deltaY < 0)
-                atan += 180;
-            if (deltaX > 0 && deltaY < 0)
-                atan -= 180;
-            if (atan < 0)
-                atan += 360;
+            var theta = Math.Atan2(deltaX, deltaY);
 
-            return atan % 360;
+            var angle = ((theta * 180 / Math.PI) + 360 % 360);
+
+            if (angle < 0)
+                angle = 360 + angle;
+            return angle;
         }
     }
 }
