5d78149761f04a2b9530738f50c5b2132a943ec1
Cpilgaard <31501420+Cpilgaard@users.noreply.github.com>
2018-04-21 14:28:18 +0200

    AltitudeDistanceCalculatorUnitTest Works CollisionAnalyzerUnitTest Does Not quite work yet

---
+++ AirTrafficMonitoringUnitTest/AltitudeDistanceCalculatorUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoringUnitTest
+{
+    [TestFixture]
+    class AltitudeDistanceCalculatorUnitTest
+    {
+        private AltitudeDistanceCalculator _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new AltitudeDistanceCalculator();
+        }
+
+        [TestCase(2000,1000,1000)]
+        [TestCase(2000, 2000, 0)]
+        [TestCase(0, 2000, 2000)]
+        [TestCase(2000, 0, 2000)]
+        public void CalculateAltitudeDistance_CalculatingAltitudeDistance_DistanceIsCorrect(int altitude1,
+            int altitude2, int expextedDistance)
+        {
+            Assert.That(_uut.CalculateAltitudeDistance(altitude1, altitude2), Is.EqualTo(expextedDistance));
+        }
+    }
+}
